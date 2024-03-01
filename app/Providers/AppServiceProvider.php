9a32a8fb982fabd5f893921f6b26ee82d61a4956
Ümit UZ <umituz998@gmail.com>
2024-03-02 03:58:53 +0700

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\Database\ConnectionService;
 use App\Services\Log\FileLogger;
 use App\Services\Log\Logger;
 use Illuminate\Support\ServiceProvider;
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(ConnectionService::class, function ($app) {
+            return ConnectionService::getInstance();
+        });
     }
 
     /**
